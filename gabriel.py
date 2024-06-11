74c06a4d73f4079461f60f2b0104a434a2ee8378
MrGSommer <110161907+MrGSommer@users.noreply.github.com>
2024-06-11 13:04:06 +0200

    Update gabriel.py
    projektfilterung gabriel

--- gabriel.py
+++ gabriel.py
@@ -5,8 +5,21 @@ import streamlit as st
 
 def speckle():
     st.title('Das Speckle Dashboard')
-    # Setze die URL, die du einbetten möchtest
-    url = 'https://app.speckle.systems/projects/99d586a085'
+    
+    # URL-Eingabefeld
+    user_url = st.text_input('Gib die Speckle URL ein:', 'https://app.speckle.systems/projects/99d586a085')
+    
+    # Dropdown-Menü für vordefinierte URLs
+    predefined_urls = {
+        'Projekt Tür': 'https://app.speckle.systems/projects/99d586a085/models/405c047b71',
+        'Projekt Fenster': 'https://app.speckle.systems/projects/99d586a085/models/ff98034292',
+        'Projekt Gesamt': 'https://app.speckle.systems/projects/99d586a085/models/2ffc61a729'
+    }
+    selected_project = st.selectbox('Wähle ein vordefiniertes Projekt:', list(predefined_urls.keys()))
+    
+    # Verwende entweder die benutzerdefinierte URL oder die vordefinierte URL
+    url = user_url if user_url else predefined_urls[selected_project]
+    
     # Erstelle einen iframe, um die Webseite einzubetten
     iframe_code = f'<iframe src="{url}" width="150%" height="1000" style="border:none;"></iframe>'
     # Zeige den iframe im Streamlit Dashboard an
