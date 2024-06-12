97480594245720ca4e5ebd5aa9f28f8057f914f3
eliawaefler <elia.waefler@gmail.com>
2024-06-12 22:00:47 +0200

    display

--- elia.py
+++ elia.py
@@ -87,8 +87,8 @@ def user_space():
             with st.form("building_form"):
                 # Fields for user to fill
                 baujahr = st.number_input('Baujahr', min_value=1900, max_value=2050, value=1990, step=1)
-                renojahr = st.number_input('Renovationsjahr (wenn vorhanden)', min_value=1900, max_value=2020, value=2024, step=1)
-                abbruchjahr = st.number_input('geplanter Rückbau: Jahr (wenn bakannt:)', min_value=1900, max_value=2200, value=2050, step=1) # noch nicht
+                renojahr = st.number_input('Renovationsjahr, wenn vorhanden', min_value=1900, max_value=2020, value=2024, step=1)
+                abbruchjahr = st.number_input('geplanter Rückbau: Jahr, wenn bekannt', min_value=1900, max_value=2200, value=2050, step=1)
                 nutzung_options = ['Wohnen', 'Gewerbe', 'Industrie', 'Landwirtschaft']
                 nutzung = st.selectbox('Nutzung', options=nutzung_options)
                 typ_options = ['Neubau', 'Abbruch', 'Umbau', 'Anderes']
@@ -99,7 +99,6 @@ def user_space():
                 speckle_link = st.text_input('speckle_link (wenn vorhanden)')
                 vol = st.text_input('Projektvolumen [m3]')
                 files = st.file_uploader("Upload Projektdokumente")
-                # Submit button
                 submitted = st.form_submit_button("Submit")
 
                 if submitted:
