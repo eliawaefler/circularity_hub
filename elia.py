2fe3d8e2e020a1877b331fdeb0cf40dec55a3457
eliawaefler <elia.waefler@gmail.com>
2024-06-12 11:59:38 +0200

    userspace update

--- elia.py
+++ elia.py
@@ -54,7 +54,12 @@ def user_space():
             st.session_state.user_space = "new"
         st.subheader("meine Projekte")
         my_projects = neon.read_db(st.secrets["NEON_URL"], "geb", condition=f"user_name = '{st.session_state.username}'")
-
+        for p in my_projects:
+            if st.button(p):
+                st.subheader(p[1])
+                st.write(p)
+                if st.button("show_matches"):
+                    show_map()
 
 
     elif st.session_state.user_space == "new":
