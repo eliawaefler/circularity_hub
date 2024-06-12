ef824874b6e78d0e3ed400c7aa0efacacf9624b6
eliawaefler <elia.waefler@gmail.com>
2024-06-12 12:22:21 +0200

    userspace update

--- elia.py
+++ elia.py
@@ -64,9 +64,9 @@ def user_space():
                 if st.button("show_matches"):
 
                     def make_match(project):
-                        all_p = neon.read_db(st.secrets["NEON_URL"], "geb")
+                        all_p = neon.read_db(st.secrets["NEON_URL"], "geb", condition=f"typ <> '{project[7]}'")
 
-                        st.write(all_p)
+                        st.write(str(all_p))
                         sorted_p = sorted(all_p, key=lambda x: x[6])
                         st.write(sorted_p)
                         return sorted_p[:3]
