260696b8c5e759bd59884a89a41372503726d904
eliawaefler <elia.waefler@gmail.com>
2024-06-17 11:50:52 +0200

    home

--- app.py
+++ app.py
@@ -88,39 +88,31 @@ def main():
     if choice == "Home":
         st.title("Circularity Hub")
         st.write("Die Plattform für zirkuläres Bauen.")
-        l, r = st.columns([1, 5])
-        with r:
+        left, right = st.columns([1, 1])
+
+        with left:
             st.markdown("""
                         ###   Das Problem
                         Die Bauindustrie ist einer der grössten Verursacher von Abfällen weltweit. Jährlich werden Tonnen von Baumaterialien verschwendet, die durchaus wiederverwendet werden könnten. Die Herausforderung liegt darin, dass es bisher keine effiziente Methode gab, die vorhandenen Ressourcen so zu koordinieren, dass Abbruchmaterialien in neuen Bauprojekten genutzt werden können.
-            """)
-        with l:
-            if st.button("Mehr erfahren"):
-                choice = "About"
-        l2, r2 = st.columns([1, 3])
-        with r2:
-            st.markdown("""
                         ###   Unsere Lösung
                         Circularity Hub ist eine innovative Plattform, die das Prinzip des zirkulären Bauens verwirklicht, indem sie Projekte, die Abbruchmaterialien benötigen, mit Neubauprojekten, die diese Materialien anbieten, effizient verbindet. Unsere fortschrittliche Matching-Technologie ermöglicht es, Bau- und Abbruchprojekte optimal aufeinander abzustimmen, sodass die Wiederverwendung von Materialien maximiert und die Abfallproduktion minimiert wird.
                         """)
-        l3, r3 = st.columns([1, 3])
-        with r3:
+
+        with right:
             st.markdown("""
+                        ###   Wie es funktioniert
                         1. **Registrierung:** Melden Sie sich an und geben Sie Informationen zu Ihrem aktuellen oder geplanten Bauprojekt ein.
                         2. **Matching:** Unsere Plattform analysiert die verfügbaren Daten und findet passende Projekte, die Ihren Bedarf an Baumaterialien decken können oder die von Ihnen angebotenen Materialien verwenden können.
                         3. **Koordination:** Nach einem erfolgreichen Match unterstützt Circularity Hub die logistische Abwicklung, um einen reibungslosen Transfer und die Wiederverwendung von Materialien zu gewährleisten.
                         """)
-        with l3:
-           if st.button("Jetzt Anmbelden"):
-               choice = "UserSpace"
-        l4, r4 = st.columns([1, 3])
-        with r4:
+            if st.button("Jetzt Anmelden"):
+                choice = "UserSpace"
             st.markdown("""
-                        ###   Wie es funktioniert
                         ###   Treten Sie unserer Bewegung bei
                         Registrieren Sie sich heute, um Teil einer nachhaltigen Bauzukunft zu sein. Reduzieren Sie Bauabfälle, sparen Sie Kosten und schützen Sie die Umwelt. Und teilen Sie Ihre Erfahrungen im Community tab.
                         """)
-        st.image(Image.open("images/circ.webp"), caption="circular building industry")
+            st.image("images/circ.webp", caption="circular building industry")
+
 
     elif choice == "test_db":
         # Connection URL for SQLAlchemy
