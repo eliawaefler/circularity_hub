a8930ed77379706751b8d473e92b221906ae8a9d
eliawaefler <elia.waefler@gmail.com>
2024-06-17 11:42:26 +0200

    home

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ def main():
         page_title="Circularity Hub", #:cyclone::hammer_and_pick::recycle:
         page_icon=":cyclone:",  # You can use emojis or path to an image file :repeat: oder :cyclone: :radio_button: :recycle: :hammer_and_pick:
         layout="wide",  # 'centered' or 'wide'
-        initial_sidebar_state="expanded"  # 'auto', 'expanded', or 'collapsed'
+        initial_sidebar_state='collapsed'  # 'auto', 'expanded', or 'collapsed' "expanded"
     )
     st.sidebar.title("Navigation")
     choice = st.sidebar.radio("Go to", ("Home", "UserSpace", "Community", "Speckle", "about")) #"test_db", "newDBtest", "Map View",
@@ -88,27 +88,42 @@ def main():
     if choice == "Home":
         st.title("Circularity Hub")
         st.write("Die Plattform für zirkuläres Bauen.")
-        l, r = st.columns(2)
+        l, r = st.columns([1, 3])
         with r:
-            st.image(Image.open("images/circ.webp"), caption="circular building industry")
-        with l:
             st.markdown("""
-                    ###   Das Problem
-                    Die Bauindustrie ist einer der grössten Verursacher von Abfällen weltweit. Jährlich werden Tonnen von Baumaterialien verschwendet, die durchaus wiederverwendet werden könnten. Die Herausforderung liegt darin, dass es bisher keine effiziente Methode gab, die vorhandenen Ressourcen so zu koordinieren, dass Abbruchmaterialien in neuen Bauprojekten genutzt werden können.
-
-                    ###   Unsere Lösung
+                                ###   Das Problem
+                                Die Bauindustrie ist einer der grössten Verursacher von Abfällen weltweit. Jährlich werden Tonnen von Baumaterialien verschwendet, die durchaus wiederverwendet werden könnten. Die Herausforderung liegt darin, dass es bisher keine effiziente Methode gab, die vorhandenen Ressourcen so zu koordinieren, dass Abbruchmaterialien in neuen Bauprojekten genutzt werden können.
+            """)
+        with l:
+            if st.button("Mehr erfahren"):
+                choice = "About"
+        l, r = st.columns([1, 3])
+        with r:
+            st.markdown("""
+                          ###   Unsere Lösung
                     Circularity Hub ist eine innovative Plattform, die das Prinzip des zirkulären Bauens verwirklicht, indem sie Projekte, die Abbruchmaterialien benötigen, mit Neubauprojekten, die diese Materialien anbieten, effizient verbindet. Unsere fortschrittliche Matching-Technologie ermöglicht es, Bau- und Abbruchprojekte optimal aufeinander abzustimmen, sodass die Wiederverwendung von Materialien maximiert und die Abfallproduktion minimiert wird.
-
-                    ###   Wie es funktioniert
+                        """)
+        with l:
+            if st.button("Mehr erfahren"):
+                choice = "About"
+        l, r = st.columns([1, 3])
+            with r:
+                st.markdown("""
                     1. **Registrierung:** Melden Sie sich an und geben Sie Informationen zu Ihrem aktuellen oder geplanten Bauprojekt ein.
                     2. **Matching:** Unsere Plattform analysiert die verfügbaren Daten und findet passende Projekte, die Ihren Bedarf an Baumaterialien decken können oder die von Ihnen angebotenen Materialien verwenden können.
                     3. **Koordination:** Nach einem erfolgreichen Match unterstützt Circularity Hub die logistische Abwicklung, um einen reibungslosen Transfer und die Wiederverwendung von Materialien zu gewährleisten.
-
-                    ###   Treten Sie unserer Bewegung bei
+                            """)
+            with l:
+                if st.button("Mehr erfahren"):
+                    choice = "About"
+                             ###   Wie es funktioniert
+                              ###   Treten Sie unserer Bewegung bei
                     Registrieren Sie sich heute, um Teil einer nachhaltigen Bauzukunft zu sein. Reduzieren Sie Bauabfälle, sparen Sie Kosten und schützen Sie die Umwelt. Und teilen Sie Ihre Erfahrungen im Community tab.
-                    """)
+
             if st.button("jetzt Anmelden!"):
                 choice = "UserSpace"
+            st.image(Image.open("images/circ.webp"), caption="circular building industry")
+
     elif choice == "test_db":
         # Connection URL for SQLAlchemy
         connection_url = st.secrets["NEON_NEW"]
